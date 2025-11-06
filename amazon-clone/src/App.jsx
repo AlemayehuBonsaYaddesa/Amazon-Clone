763943627980db08d46893a832350eb26008fa47
AlemayehuBonsaYaddesa <150921885+AlemayehuBonsaYaddesa@users.noreply.github.com>
2025-11-06 18:25:32 +0300

    header

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 function App() {
   return (
     <>
-      <h1>Amazon-Clone</h1>
+      <Header />
     </>
   );
 }
---
+++ amazon-clone/src/Components/Header/Header.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+// import Logo from "../../../public/Logo.png";
+import classes from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { IoSearch } from "react-icons/io5";
+import LowerHeader from "./LowerHeader";
+import { SlLocationPin } from "react-icons/sl";
+import { BiCart } from "react-icons/bi";
+
+function Header() {
+  return (
+    <section className={classes.fixed}>
+      <section>
+        <div className={classes.header__container}>
+          <div className={classes.logo__container}>
+            <Link to="/">
+              {/* Amazon Logo */}
+              <img
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="Amazon"
+              />
+            </Link>
+
+            {/* Delivery */}
+            <div className={classes.delivery}>
+              <div>Delivered to</div>
+              <SlLocationPin />
+            </div>
+          </div>
+
+          <div className={classes.search}>
+            {/* Search bar */}
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" name="" placeholder="Search product" />
+            {/* Icon */}
+            <IoSearch />
+          </div>
+
+          {/* right side link */}
+          <div className={classes.order__container}>
+            <Link to="" className={classes.language}>
+              <img
+                src="https://www.shutterstock.com/image-photo/us-american-flag-full-frame-260nw-2609455371.jpg"
+                alt=""
+              />
+              <select name="" id="">
+                <option value="">EN</option>
+              </select>
+            </Link>
+
+            {/* links */}
+            <Link to="/auths">
+              <div>
+                <p>Sign In</p>
+                <span>Account & Lists</span>
+              </div>
+            </Link>
+
+            {/* Orders */}
+            <Link to="/orders">
+              <p>Returns</p>
+              <span>& Orders</span>
+            </Link>
+
+            {/* Cart */}
+            {/* Cart */}
+            <Link to="/carts" className={classes.cart}>
+              <BiCart size={35} />
+              <span>{/* { basket.length } */} 0</span>
+            </Link>
+          </div>
+        </div>
+      </section>
+      <LowerHeader />
+    </section>
+  );
+}
+
+export default Header;
