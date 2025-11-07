a744f9099eab6cf87d3e2deed70a82af7104a53d
AlemayehuBonsaYaddesa <150921885+AlemayehuBonsaYaddesa@users.noreply.github.com>
2025-11-07 15:54:29 +0300

    Category part

---
+++ amazon-clone/src/Components/Product/Product.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "./ProductCard";
+import classes from "./Product.module.css";
+
+function Product() {
+  const [products, setProduct] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products`)
+      .then((resp) => setProduct(resp.data))
+      .catch((error) => console.log(error));
+  }, []);
+  // console.log(products);
+  return (
+    <section className={classes.product__container}>
+      {products &&
+        products.map((singleProduct, i) => {
+          return <ProductCard product={singleProduct} key={i} />;
+        })}
+    </section>
+  );
+}
+
+export default Product;
